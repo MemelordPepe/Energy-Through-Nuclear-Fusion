64228abf14188691deb534488c8fe2623a1751dd
Corramel <cutelestiaisdebestia@hotmail.com>
2015-11-18 08:01:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    var originalFont = $('html').css('font-size');
+    var originalFontNum = parseFloat(originalFont);
+    var increasedFont = originalFontNum * 1.3;
+    $('#nicememe').hide();
+    $('#nicememe').toggle(1000);
+   $('.Dankmeme').mouseover(function() {
+       $('html').css('font-size', increasedFont)
+   }); 
+   $('.Dankmeme').mouseleave(function() {
+       $('html').css('font-size', originalFont)
+   });
 });
